cc30d1c2372d0af4b2e7b45714108fe4938fc1d6
Janos G. Hajagos <risk.limits@gmail.com>
2013-06-27 18:22:13 -0400

    cleaned up this code before pushing to the server

--- example_graph_extraction.py
+++ example_graph_extraction.py
@@ -6,7 +6,6 @@ def main():
     extract_provider_network("state = 'HI'", file_name_prefix="hi_")
     extract_provider_network("zip like '11215%' or zip like '11212%'", file_name_prefix="brooklyn_sample")
     extract_provider_network("city like 'Brooklyn%' and state = 'NY'", add_leaf_nodes=False, file_name_prefix="brooklyn_core")
-    extract_provider_network("npi in ('1801180138','1033149372','1679795181','109522833')", add_leaf_to_leaf_edges=False,file_name_prefix="fraud_case")
     main("npi='1275534935'", add_leaf_nodes=True, add_leaf_to_leaf_edges=True, file_name_prefix="chiro_investigation")
 
 
